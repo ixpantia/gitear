d5672b34ce503532e8eed2a0730e0638e781afcc
fransacida <francisco.sacida@delaguayaba.com>
2018-12-07 16:37:03 -0600

    Extraccion de lista de teams

---
+++ R/get-list-an-org-teams.R
@@ -0,0 +1,39 @@
+#' @import httr
+#' @import jsonlite
+#'
+#' @description Get list an organization's teams
+#' @title Return list an organization's teams
+#' 
+#' @param base_url The base URL for your gitea server (no trailing '/')
+#' @param api_key The user's API token key for the gitea service
+#'
+#' @param org Name of the organization to get
+#' 
+#'@export
+get_list_org_teams <- function(base_url, api_key, org){
+    if (missing(base_url)) {
+        warning("Please add a valid URL")
+    } else if (missing(api_key)) {
+        warning("Please add a valid API token")
+    } else if (missing(org)) {
+        warning("Please add a valid name of the organization")
+    }else
+        try({
+            base_url <- sub("/$", "", base_url)
+            gitea_url <- file.path(base_url, "api/v1",
+                                   sub("^/", "", "/orgs"), org,"teams") 
+            
+            authorization <- paste("token", api_key)
+            r <- GET(gitea_url, add_headers(Authorization = authorization),
+                     accept_json())
+            
+            # To convert http errors to R errors
+            stop_for_status(r)
+            
+            cont_list_an_org_teams <- content(r, as = "text")
+            cont_list_an_org_teams <- fromJSON(cont_list_an_org_teams)
+            cont_list_an_org_teams <- as.data.frame(cont_list_an_org_teams)
+            
+            return(cont_list_an_org_teams)
+        })
+}
